4aaeadc440850f5e3941e68c0f65c5ee867ae7d2
techbyadi <pssharma.aditya@gmail.com>
2024-07-21 13:03:09 -0700

    Add next button to display all the questions

--- js/app.js
+++ js/app.js
@@ -19,6 +19,16 @@ const sports = [
     options: ['Australia', 'India', 'England', 'Pakistan'],
     answer: 'India',
   },
+  {
+    question: 'Which golfer is known for winning the Masters Tournament with a record 12-stroke margin in 1997?',
+    options: ['Jack Nicklaus', 'Phil Mickelson', 'Tiger Woods', 'Arnold Palmer'],
+    answer: "Tiger Woods",
+  },
+  {
+  question: 'Which female tennis player has won the most Grand Slam singles titles?',
+  options: ['Serena Williams', 'Steffi Graf', 'Margaret Court', 'Martina Navratilova'],
+  answer: 'Margaret Court',
+  }
 ];
 
 /*-------------------------------- Constants --------------------------------*/
@@ -37,6 +47,8 @@ let sportsButtonEl = document.getElementById("sports");
 let literatureButtonEl = document.getElementById("literature");
 let headerTextEl = document.querySelector(".h4");
 let mainSectionEl = document.querySelector(".category");
+let footerSectionEl = document.querySelector('.footer');
+let sectionEl = document.querySelector('.main');
 
 /*-------------------------------- Functions --------------------------------*/
 
@@ -51,16 +63,23 @@ function setHeaderText(text) {
 }
 
 function displayQuestion() {
+  if(counterSports <= sports.length-1)
   headerTextEl.textContent = sports[counterSports].question;
 }
 
 function displayOptions() {
-  for (let i = 0; i <= sports[0].options.length - 1; i++) {
+  if(counterSports <= sports.length-1){
+  for (let option = 0; option <= sports[0].options.length - 1; option++) {
     let optionsEl = document.createElement("button");
     mainSectionEl.appendChild(optionsEl);
     optionsEl.className = "options";
-    optionsEl.textContent = sports[counterSports].options[i];
+    optionsEl.textContent = sports[counterSports].options[option];
   }
+}
+}
+
+function clearOptions(){
+  mainSectionEl.innerHTML='';
 }
 
 function handleSportsCategoryClick() {
@@ -70,13 +89,42 @@ function handleSportsCategoryClick() {
   headerTextEl.classList.toggle("question");
   displayQuestion();
   displayOptions();
+  addNextButton();
 }
 
-function handleOptionsClick(){
-  counterSports++;
+function addNextButton(){
+  let nextButtonEl = document.createElement("button");
+  footerSectionEl.appendChild(nextButtonEl);
+  nextButtonEl.className = 'next';
+  nextButtonEl.textContent = 'Next'
 }
+
+function renderQnAState(){
+  displayQuestion();
+  clearOptions();
+  displayOptions();
+}
+
+
+function renderLastQuestionState(){
+  clearOptions();
+}
+
+function handleNextButtonClick(event) {
+  if (event.target.classList.contains("next")) {
+    counterSports++;
+    renderQnAState();
+    if (counterSports === sports.length - 1) {
+      event.target.textContent = "Result";
+    }
+    if (counterSports === sports.length) {
+      sectionEl.innerHTML = "";
+    }
+  }
+}
+
 
 /*----------------------------- Event Listeners -----------------------------*/
 
 sportsButtonEl.addEventListener("click", handleSportsCategoryClick);
-mainSectionEl.addEventListener('click', handleOptionsClick);
+footerSectionEl.addEventListener('click', handleNextButtonClick);
